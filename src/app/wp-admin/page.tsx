cab6c60c391a1bcd2c09d39e4ff54480b446b5c8
Z User <z@container>
2026-07-18 02:12:15 +0000

    Add WordPress admin at /wp-admin + reset all button

---
+++ src/app/wp-admin/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import * as React from "react";
+import { useRouter } from "next/navigation";
+import { WordPressAdmin } from "@/components/playbeat/wordpress-admin";
+
+export default function WpAdminPage() {
+  return <WordPressAdmin />;
+}
